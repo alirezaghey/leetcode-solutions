a03277108d100dbeaa8ae26b827f940aac179678
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-09-19 14:46:52 +0200

    minor English improvement

--- js/merge-k-sorted-lists.js
+++ js/merge-k-sorted-lists.js
@@ -4,12 +4,12 @@
 
 /*
 Initial thoughts:
-We are going to loop over every linked list and check which of them
+We are going to loop over every linked lists and check which of them
 has the smallest value and weave our way forward until we have reached
 the end of every one of the linked lists.
 
 Time complexity: O(k * n) where k === the number of linked lists and n is the number of all the nodes in all the linked lists
-Space complexity: O(k) where k === the number of linked lists because we are going to hold the pointer to the beginning of each
+Space complexity: O(k) where k === the number of linked lists because we are going to hold a pointer to the beginning of each
 linked list in it and advance it if we merge that particular node. 
 */
 

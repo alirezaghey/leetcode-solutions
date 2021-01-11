204672867f70e5b47f19832de696fb8f1ef9ef02
alirezaghey <alireza.q1357@gmail.com>
2021-01-11 14:51:30 +0100

    add Dijkstra solution to Mimimum Cost to Make Valid Path problem

--- go/min_cost_make_valid_path_in_grid.go
+++ go/min_cost_make_valid_path_in_grid.go
@@ -1,6 +1,79 @@
 package leetcode
 
 import (
+	"container/heap"
+	"strconv"
+)
+
+// modified Dijkstra using heap
+func minCost(grid [][]int) int {
+	RIGHT, LEFT, DOWN, UP := 1, 2, 3, 4
+	R, C := len(grid), len(grid[0])
+	dirs := [][]int{{-1, 0, UP}, {0, 1, RIGHT}, {1, 0, DOWN}, {0, -1, LEFT}}
+
+	dic := make(map[string]int)
+	dic["0,0"] = 0
+	hp := make(NodeHeap, 0)
+	heap.Push(&hp, Node{0, 0, 0})
+
+	for hp.Len() > 0 {
+		node := heap.Pop(&hp).(Node)
+		r, c, cost := node.r, node.c, node.cost
+
+		if r == R-1 && c == C-1 {
+			return cost
+		}
+
+		for _, next := range dirs {
+			dr, dc, d := next[0], next[1], next[2]
+			nr, nc := r+dr, c+dc
+
+			if nr < 0 || nr >= R || nc < 0 || nc >= C {
+				continue // coords out of bounds
+			}
+
+			var ncost int
+			if grid[r][c] == d {
+				ncost = cost
+			} else {
+				ncost = cost + 1
+			}
+
+			key := strconv.Itoa(nr) + "," + strconv.Itoa(nc)
+			if val, ok := dic[key]; ok && val <= ncost {
+				continue // already visited with lower cost
+			}
+			dic[key] = ncost
+			heap.Push(&hp, Node{nr, nc, ncost})
+		}
+	}
+	return 0
+}
+
+type Node struct {
+	r, c, cost int
+}
+
+type NodeHeap []Node
+
+func (nh NodeHeap) Len() int { return len(nh) }
+func (nh NodeHeap) Less(i, j int) bool {
+	return nh[i].cost < nh[j].cost
+}
+func (nh NodeHeap) Swap(i, j int) { nh[i], nh[j] = nh[j], nh[i] }
+func (nh *NodeHeap) Push(x interface{}) {
+	*nh = append(*nh, x.(Node))
+}
+func (nh *NodeHeap) Pop() interface{} {
+	n := len(*nh)
+	x := (*nh)[n-1]
+	*nh = (*nh)[:n-1]
+	return x
+}
+
+// BFS
+/*
+import (
 	"container/list"
 	"strconv"
 )
@@ -54,3 +127,4 @@ func minCost(grid [][]int) int {
 type Node struct {
 	r, c, cost int
 }
+*/
--- python/min-cost-make-valid-path-in-grid.py
+++ python/min-cost-make-valid-path-in-grid.py
@@ -1,5 +1,7 @@
-from collections import deque
+from collections import deque, heapq
+
 class Solution:
+    # BFS
     def minCost(self, grid: List[List[int]]) -> int:
         RIGHT, LEFT, DOWN, UP = range(1, 5)
         R, C = len(grid), len(grid[0])
@@ -23,3 +25,25 @@ class Solution:
                 deq.append((nr, nc, new_cost))
                 
         return best
+    
+    # Modified Dijkstra using heap
+    def minCost2(self, grid: List[List[int]]) -> int:
+        RIGHT, LEFT, DOWN, UP = range(1, 5)
+        R, C = len(grid), len(grid[0])
+        
+        dic = {(0, 0): 0}
+        hp = [(0, 0, 0)]
+        
+        while hp:
+            cost, r, c = heapq.heappop(hp)
+            r, c = abs(r), abs(c)
+            if r == R-1 and c == C-1:
+                return cost
+            
+            for nr, nc, direc in ((r-1, c, UP), (r, c+1, RIGHT),
+                                    (r+1, c, DOWN), (r, c-1, LEFT)):
+                if nr < 0 or nr >= R or nc < 0 or nc >= C: continue # coords out of bound
+                new_cost = cost+1 if direc != grid[r][c] else cost # cost increase if direction differs
+                if (nr, nc) in dic and dic[(nr,nc)] <= new_cost: continue # we have visited this cell with smaller cost
+                dic[(nr, nc)] = new_cost
+                heapq.heappush(hp, (new_cost, -nr, -nc))

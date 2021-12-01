eb02b480ebab87f090d80d52a062306d339f223f
alirezaghey <alireza.q1357@gmail.com>
2021-12-01 13:20:57 +0100

    add skyline approach to Maximum Rectangle solution

--- go/maximal_rectangle.go
+++ go/maximal_rectangle.go
@@ -1,6 +1,66 @@
 package leetcode
 
+/*
+Approach:
+Using the max area rectangle in skyline approach
+with a mono stack
+*/
 func maximalRectangle(matrix [][]byte) int {
+	R := len(matrix)
+	if R == 0 {
+		return 0
+	}
+	C := len(matrix[0])
+
+	dp := make([][]int, R)
+	for r := 0; r < R; r++ {
+		dp[r] = make([]int, C+1)
+	}
+
+	for c := 0; c < C; c++ {
+		curr := 0
+		for r := 0; r < R; r++ {
+			if matrix[r][c] == '0' {
+				curr = 0
+				continue
+			}
+			curr++
+			dp[r][c] = curr
+		}
+	}
+
+	res := 0
+	for r := 0; r < R; r++ {
+		stack := []int{}
+		for c := 0; c < C+1; c++ {
+			for len(stack) > 0 && dp[r][stack[len(stack)-1]] >= dp[r][c] {
+				h := dp[r][stack[len(stack)-1]]
+				stack = stack[:len(stack)-1]
+				var w int
+				if len(stack) > 0 {
+					w = c - stack[len(stack)-1] - 1
+				} else {
+					w = c
+				}
+				if w*h > res {
+					res = w * h
+				}
+			}
+			stack = append(stack, c)
+		}
+	}
+	return res
+}
+
+/*
+Approach:
+Create dp where dp[i][j] is the sum of continues 1s to the left in row i including for j itself
+For each column of the dp, for each row, look for continues rows without zero, take their min times the count of rows
+That's the maximum area we can achieve
+Note that we need to start at each row once
+Also, we can break if we find a row that is zero
+*/
+func maximalRectangle2(matrix [][]byte) int {
 	R := len(matrix)
 	if R == 0 {
 		return 0
--- python/maximal-rectangle.py
+++ python/maximal-rectangle.py
@@ -2,12 +2,45 @@ from typing import List
 
 class Solution:
     # Approach:
+    # Using the max area rectangle in skyline approach
+    # with a mono stack
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        R = len(matrix)
+        if R == 0: return 0
+        C = len(matrix[0])
+        
+        dp = [[0]*(C) for _ in range(R)]
+        
+        for c in range(C):
+            curr = 0
+            for r in range(R):
+                if matrix[r][c] == "0":
+                    curr = 0
+                    continue
+                else:
+                    curr += 1
+                    dp[r][c] = curr
+        
+        res = 0
+        for r in range(R):
+            dp[r].append(0)
+            stack = [-1]
+            for c in range(C+1):
+                while stack and dp[r][stack[-1]] > dp[r][c]:
+                    h = dp[r][stack.pop()]
+                    w = c - stack[-1] - 1
+                    res = max(res, h*w)
+                stack.append(c)
+        return res
+
+        
+    # Approach:
     # Create dp where dp[i][j] is the sum of continues 1s to the left in row i including for j itself
     # For each column of the dp, for each row, look for continues rows without zero, take their min times the count of rows
     # That's the maximum area we can achieve
     # Note that we need to start at each row once
     # Also, we can break if we find a row that is zero
-    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+    def maximalRectangle2(self, matrix: List[List[str]]) -> int:
             R = len(matrix)
             if R == 0: return 0
             C = len(matrix[0])

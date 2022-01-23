0263a7fa2d020e03d2bf9b59fa180b6ea246fee9
alirezaghey <alireza.q1357@gmail.com>
2022-01-23 12:41:27 +0100

    add iterative Python solution for Sequential Digits

--- python/sequential-digits.py
+++ python/sequential-digits.py
@@ -1,8 +1,19 @@
 from typing import List
-
+import math
 
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
+        base = "123456789"
+        low_d, high_d = int(math.log10(low))+1, int(math.log10(high))+1
+        res = []
+        for length in range(low_d, high_d+1):
+            for i in range(9-length+1):
+                if low <= (num := int(base[i:i+length])) <= high:
+                    res.append(num)
+        return res
+
+
+    def sequentialDigits2(self, low: int, high: int) -> List[int]:
         n = len(str(high))
         def backtrack(num, curr, res):
             if len(curr) > n:

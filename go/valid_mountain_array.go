84cd86ad371f6c80f493e473472c148f7c16fad6
alirezaghey <alireza.q1357@gmail.com>
2022-01-25 11:24:39 +0100

    add alternative Go solution for Valid Mountain Array

--- go/valid_mountain_array.go
+++ go/valid_mountain_array.go
@@ -2,7 +2,35 @@ package leetcode
 
 // Time complexity: O(n)
 // Space complexity: O(1)
+// Alternative solution
 func validMountainArray(arr []int) bool {
+	N := len(arr)
+	if N < 3 {
+		return false
+	}
+
+	peaks := 0
+	for i := 1; i < N-1; i++ {
+		if arr[i] == arr[i-1] || arr[i] == arr[i+1] {
+			return false
+		}
+		if arr[i] < arr[i-1] && arr[i] < arr[i+1] {
+			return false
+		}
+		if arr[i] > arr[i-1] && arr[i] > arr[i+1] {
+			peaks++
+		}
+		if peaks > 1 {
+			return false
+		}
+	}
+	return peaks == 1
+
+}
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+func validMountainArray2(arr []int) bool {
 	if len(arr) < 3 {
 		return false
 	}

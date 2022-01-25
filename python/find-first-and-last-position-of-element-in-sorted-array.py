a767b401feb808de5e0df107aa836c14d4452b3d
alirezaghey <alireza.q1357@gmail.com>
2022-01-25 12:09:00 +0100

    add optimized Python solutions for problem Nr. 34
    cleanup of previous solutions

--- python/find-first-and-last-position-of-element-in-sorted-array.py
+++ python/find-first-and-last-position-of-element-in-sorted-array.py
@@ -20,10 +20,72 @@
 # Space complexity: O(log n) because of the recursive binary search
 
 from typing import List
+import bisect
 
-
+# Note: Everything after searchRange2 is old and not optimal
+# I realize that I've learned some lessons over the past couple of years
 class Solution:
+
+    # Time complexity: O(log n) where n is the length of nums
+    # Space complexity: O(1)
+    # Implementing my own bisect_left/bisect_right functions
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def bisect_left(arr, target):
+            left, right = 0, len(arr)
+            
+            while left < right:
+                mid = left + (right - left) // 2
+                if target > arr[mid]:
+                    left = mid+1
+                else:
+                    right = mid
+            return left
+        
+        def bisect_right(arr, target):
+            left, right = 0, len(arr)
+            
+            while left < right:
+                mid = left + (right - left) // 2
+                if target < arr[mid]:
+                    right = mid
+                else:
+                    left = mid+1
+            return left
+        
+        if len(nums) == 0: return [-1, -1]
+        left = bisect_left(nums, target)
+        if left >= len(nums) or nums[left] != target:
+            return [-1, -1]
+        
+        right = bisect_right(nums, target)
+        return [left, right-1]
+        
+    # Time complexity: O(log n) where n is the length of nums
+    # Space compleixty: O(1)
+    # Using pythons built-in bisect module
+    def searchRange2(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) == 0: return [-1, -1]
+        
+        left = bisect.bisect_left(nums, target)
+        if left >= len(nums) or nums[left] != target:
+            return [-1, -1]
+        right = bisect.bisect_right(nums, target)
+        return [left, right-1]
+
+
+
+    def searchRange3(self, nums: List[int], target: int) -> List[int]:
+        def BinarySearch(nums: List[int], target: int, low: int, high: int) -> int:
+            if low > high:
+                return -1
+            mid = low + (high-low)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                return BinarySearch(nums, target, low, mid-1)
+            else:
+                return BinarySearch(nums, target, mid+1, high)
+
         index = BinarySearch(nums, target, 0, len(nums)-1)
         if index == -1:
             return [-1, -1, ]
@@ -41,27 +103,25 @@ class Solution:
         return [left, right]
 
 
-def BinarySearch(nums: List[int], target: int, low: int, high: int) -> int:
-    if low > high:
-        return -1
-    mid = low + (high-low)//2
-    if nums[mid] == target:
-        return mid
-    elif nums[mid] > target:
-        return BinarySearch(nums, target, low, mid-1)
-    else:
-        return BinarySearch(nums, target, mid+1, high)
 
+    # Optimization:
+    # Using a iterative binary search we can render the space complexity constant
+    # although this will arguably have a less readable code
 
-# Optimization:
-# Using a iterative binary search we can render the space complexity constant
-# although this will arguably have a less readable code
+    # Time Complexity: O(log n)
+    # Space Complexity: O(1)
+    def searchRange4(self, nums: List[int], target: int) -> List[int]:
+        def BinarySearchIterative(nums: List[int], target: int, low: int, high: int) -> int:
+            while low <= high:
+                mid = low + (high-low)//2
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] > target:
+                    high = mid-1
+                else:
+                    low = mid+1
+            return -1
 
-# Time Complexity: O(log n)
-# Space Complexity: O(1)
-
-class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
         index = BinarySearchIterative(nums, target, 0, len(nums))
         if index == -1:
             return [-1, -1]
@@ -79,13 +139,3 @@ class Solution:
         return [left, right]
 
 
-def BinarySearchIterative(nums: List[int], target: int, low: int, high: int) -> int:
-    while low <= high:
-        mid = low + (high-low)//2
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] > target:
-            high = mid-1
-        else:
-            low = mid+1
-    return -1

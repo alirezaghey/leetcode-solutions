fe87609c96a1f252d0d4bb176c0cd593abfec808
alirezaghey <alireza.q1357@gmail.com>
2022-01-28 18:16:39 +0100

    add constant space Go solution for Backspace String Compare

--- go/backspace_string_compare.go
+++ go/backspace_string_compare.go
@@ -1,5 +1,44 @@
 package leetcode
 
+// Time complexity: O(n + m) where n and m are the lengths of s and t
+// Space complexity: O(1)
+func backspaceCompare(s string, t string) bool {
+	i, j := len(s)-1, len(t)-1
+
+	for i >= 0 || j >= 0 {
+		c := 0
+		for i >= 0 && (s[i] == '#' || c > 0) {
+			if s[i] == '#' {
+				c++
+			} else {
+				c--
+			}
+			i--
+		}
+
+		c = 0
+		for j >= 0 && (t[j] == '#' || c > 0) {
+			if t[j] == '#' {
+				c++
+			} else {
+				c--
+			}
+			j--
+		}
+
+		if i >= 0 && j >= 0 {
+			if s[i] != t[j] {
+				return false
+			}
+		} else {
+			break
+		}
+		i--
+		j--
+	}
+	return i < 0 && j < 0
+}
+
 // Time complexity: O(n + m) where n and m are the length of s and t
 // Space complexity: O(n + m)
 func backspaceCompare2(s string, t string) bool {
@@ -26,5 +65,4 @@ func backspaceCompare2(s string, t string) bool {
 		}
 	}
 	return true
-
 }

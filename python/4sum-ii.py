18390961153a809c1b1830dba8803e6e09cbfb11
alirezaghey <alireza.q1357@gmail.com>
2022-02-03 10:35:47 +0100

    add different Python solution for 4Sum II

--- python/4sum-ii.py
+++ python/4sum-ii.py
@@ -1,10 +1,19 @@
 from typing import List
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 class Solution:
     # Time complexity: O(n^2) where n is the length of nums
     # Space complexity: O(n^2)
+    # Using python's Counter and sum
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        ct = Counter(x+y for x in nums1 for y in nums2)
+        res = sum(ct[-(x+y)] for x in nums3 for y in nums4)
+        return res
+
+
+    # Time complexity: O(n^2) where n is the length of nums
+    # Space complexity: O(n^2)
+    def fourSumCount2(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
         res = 0
         dic = defaultdict(int)
         for x, y in ((x, y) for x in nums1 for y in nums2):
